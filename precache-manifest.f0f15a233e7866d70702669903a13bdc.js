d0f4ae389006888ff27458bad750f7ac77da708a
Dakshayani-koppuravuri <dakshayanikoppuravuri2949@gmail.com>
2020-07-22 22:33:12 +0530

    Updates

---
+++ precache-manifest.f0f15a233e7866d70702669903a13bdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232f0ead5c8d58f378db2a665da9abdf",
+    "url": "/TodoList-React/index.html"
+  },
+  {
+    "revision": "de2308a6d2d05f797d2c",
+    "url": "/TodoList-React/static/css/main.1fad71d4.chunk.css"
+  },
+  {
+    "revision": "61fe3182e5e51535312e",
+    "url": "/TodoList-React/static/js/2.81fe4ddd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TodoList-React/static/js/2.81fe4ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de2308a6d2d05f797d2c",
+    "url": "/TodoList-React/static/js/main.c9b89acf.chunk.js"
+  },
+  {
+    "revision": "86b89d7719699d5f5a42",
+    "url": "/TodoList-React/static/js/runtime-main.a2c6de00.js"
+  },
+  {
+    "revision": "51b3ace34de45a950385bea7d6c891ae",
+    "url": "/TodoList-React/static/media/background.51b3ace3.jpg"
+  }
+]);
